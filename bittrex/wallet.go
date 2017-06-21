3b9c0847d2d7819b3da1c49ec9f62deabe5123a0
maciejspychala <mac.spychala@gmail.com>
2017-06-21 19:17:28 +0000

    add concurrency when query for coin price

--- bittrex/wallet.go
+++ bittrex/wallet.go
@@ -5,7 +5,7 @@ import (
     "encoding/json"
 )
 
-type wallet struct {
+type Wallet struct {
     Currency string
     Balance float64
     Available float64
@@ -13,11 +13,11 @@ type wallet struct {
     CryptoAddress string
 }
 
-func (w *wallet) ToString() (text string) {
+func (w *Wallet) ToString() (text string) {
     return fmt.Sprintf("%s\t%12.8f", w.Currency, w.Balance)
 }
 
-func (c *Client) GetWallets() (wallets []wallet, e error) {
+func (c *Client) GetWallets() (wallets []Wallet, e error) {
     response, err := c.get("/account/getbalances", nil, "1.1")
     err = json.Unmarshal(response.Result, &wallets)
     return wallets, err
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     . "bitcoin/bittrex"
     "fmt"
+    "sync"
     "strings"
     "io/ioutil"
 )
@@ -14,22 +15,35 @@ func loadCredentials() (apiKey, secret string) {
     return cred[0], cred[1]
 }
 
+func getCoinInfo(w Wallet, client *Client, ch chan<- float64) {
+    var btcValue float64
+    if w.Currency == "BTC" {
+        btcValue = w.Balance
+    } else {
+        t, _ := client.GetTick("BTC-" + w.Currency)
+        btcValue = t.Last * w.Balance
+    }
+    printMutex.Lock()
+    fmt.Printf("%s\t%12.8f\n", w.ToString(), btcValue)
+    printMutex.Unlock()
+    ch<- btcValue
+}
+
+var printMutex sync.Mutex
+
 func main() {
     key, secret := loadCredentials()
     client := NewClient(key, secret)
     wallets, _ := client.GetWallets()
+    ch := make(chan float64)
     var wholeWalletValue float64
     fmt.Printf("%s\t%12s\t%12s\n", "name", "balance", "btc value")
     for _, w := range wallets {
-        var btcValue float64
-        if w.Currency == "BTC" {
-            btcValue = w.Balance
-        } else {
-            t, _ := client.GetTick("BTC-" + w.Currency)
-            btcValue = t.Last * w.Balance
-        }
-        fmt.Printf("%s\t%12.8f\n", w.ToString(), btcValue)
-        wholeWalletValue += btcValue
+        go getCoinInfo(w, client, ch)
+    }
+    for i := 0; i < len(wallets); i++ {
+        value := <-ch
+        wholeWalletValue += value
     }
     fmt.Printf("\nwallet value : %12.8f btc\n", wholeWalletValue)
 }
